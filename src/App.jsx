94ae62b7137b658ada244cc4e97e359da59f313c
marksketch <markssketch@gmail.com>
2020-10-05 10:03:02 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Heading from "./components/Heading"
+
+function App(){
+    return <section className="inner">
+        <Heading />
+        <div className="flex">
+
+        </div>
+    </section>
+}
+
+export default App
---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Heading(){
+    return <header>
+        Choose what you would like to do
+    </header>
+}
+
+export default Heading
